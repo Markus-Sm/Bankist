324d9b9fe72cb9810386bea63432735bf812bf52
Marek Samofal <markus@Mareks-MacBook-Air.local>
2023-06-24 22:30:22 +0200

    docs: Implementing Smooth Scrolling

--- bankist/script.js
+++ bankist/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,87 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/*===================================*/
+/*===================================*/
+/*===================================*/
+
+// Selecting elements
+console.log(document.documentElement);
+
+const header = document.querySelector('.header');
+
+const allSection = document.querySelectorAll('.section');
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+
+// Creating and inserting elements
+// insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = "We use cookied for improved functionality and analytics.";
+message.innerHTML =
+  `We use cookied for improved functionality and analytics. <button class='btn btn--close-cookie'>Go it!</button>`;
+
+// header.prepend(message);
+header.append(message);
+
+document
+  .querySelector('.btn--close-cookie').addEventListener('click', function() {
+    message.remove();
+  });
+  console.log(message)
+
+  // Style
+
+  message.style.backgroundColor = "#37383d";
+  message.style.width = '120%';
+  message.style.padding = '10px 0';
+
+  // document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+  // Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+
+// Data attributes
+console.log(logo.dataset.harmony)
+
+/*===================================*/
+/*== Implementing Smooth Scrolling ==*/
+/*===================================*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener("click", function(e) {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log('height/width viewport', 
+  document.documentElement.clientHeight,
+  document.documentElement.clientWidth);
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth'});
+});
+
